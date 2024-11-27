61ed2b803c58851e3ed8c7f1a5f3c980e52c7282
Cristian Alexander Sierra <cristiansierra.rap@gmail.com>
2024-11-27 00:43:08 -0400

    feat: Class settings now can be exported

--- src/config.py
+++ src/config.py
@@ -1,4 +1,9 @@
 from pydantic_settings import BaseSettings
 
+#Variables declared in .env are defined here to be used. The variable in .env must be the same as the one that 
+# will be placed in the class 
 class Settings(BaseSettings):
     URL_DATABASE: str
+
+
+settings = Settings()
