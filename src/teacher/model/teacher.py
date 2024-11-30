cf0d5f298046a6f08c5f5893ca0ad92062a8ea35
Ezequiel Andres <ezequiel@djobnet.com>
2024-11-29 20:19:06 -0400

    feat: Connection database.

--- src/teacher/model/teacher.py
+++ src/teacher/model/teacher.py
@@ -1,11 +1,29 @@
+from src.database import Base
+from sqlalchemy import String, Integer, Boolean, ForeignKey, Column
 from pydantic import BaseModel
-from typing import Optional
 
-class Teacher(BaseModel):
-    id: Optional[str] = None
-    first_name: Optional[str] = None
-    last_name: Optional[str] = None
+class Teacher(Base):
+    __tablename__ = "teachers"
+    id = Column(Integer, primary_key = True, index = True)
+    first_name = Column(String, index = True)
+    last_name = Column(String, index = True)
+    model_config = {
+        "from_attributes": True,
+        "arbitrary_types_allowed": True,
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "first_name": "Juan",
+                    "last_name": "Pedro"
+                },
+            ]
+        }
+    }
 
+
+class CreateTeacherRequest(BaseModel):
+    first_name = str
+    last_name = str
     model_config = {
         "from_attributes": True,
         "arbitrary_types_allowed": True,
