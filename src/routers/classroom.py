347090580d5d365827c4c9787818c9dd05968ee1
Ezequiel Bermudez <ezequiel@djobnet.com>
2025-02-03 21:22:15 -0400

    fix: classroom router

--- src/routers/classroom.py
+++ src/routers/classroom.py
@@ -1,44 +1,49 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from typing import List
 from src.models.classroom import Classroom, CreateClassroomRequest
 from src.services.classroom import ClassroomService
+from sqlalchemy.orm import Session
+from src.database import get_db
 
-
-Classroom_service = ClassroomService
 
 router = APIRouter(
     prefix="/Classroom",
     tags=['Classrooms']
 )
-# Crear un nuevo salón
+
 
 @router.post("", status_code=201)
-def crear_Classroom(Classroom: CreateClassroomRequest):
+def crear_Classroom(Classroom: CreateClassroomRequest, db: Session = Depends(get_db)):
+    Classroom_service = ClassroomService(db)
     respuesta = Classroom_service.create_Classroom(Classroom)
     return respuesta
 
-# Leer todos los Classroomes
+
 @router.get("")
-def obtener_Classroomes():
+def obtener_Classroomes(db: Session = Depends(get_db)):
+    Classroom_service = ClassroomService(db)
     respuesta = Classroom_service.get_all_Classroomes()
     return respuesta
 
-# Leer un salón por ID
+
 @router.get("/{Classroom_id}")
-def obtener_Classroom(Classroom_id: int):
+def obtener_Classroom(Classroom_id: int, db: Session = Depends(get_db)):
+    Classroom_service = ClassroomService(db)
     respuesta = Classroom_service.get_Classroom_by_id(Classroom_id)
     if respuesta:
         return respuesta
     raise HTTPException(status_code=404, detail="Salón no encontrado")
 
-# Actualizar un salón
+
 @router.put("/{Classroom_id}")
-def actualizar_Classroom(Classroom_id: int, Classroom: CreateClassroomRequest):
+def actualizar_Classroom(Classroom_id: int, Classroom: CreateClassroomRequest, db: Session = Depends(get_db)):
+    Classroom_service = ClassroomService(db)
     respuesta = Classroom_service.update_Classroom(Classroom_id, Classroom)
     return respuesta
 
-# Borrar un salón
+
 @router.delete("/{Classroom_id}")
-def borrar_Classroom(Classroom_id: int):
+def borrar_Classroom(Classroom_id: int, db: Session = Depends(get_db)):
+    Classroom_service = ClassroomService(db)
     respuesta = Classroom_service.delete_Classroom(Classroom_id)
     return respuesta
