cb93bb186945d557ca1ff45de3a4531f5562ab95
Ezequiel Bermudez <ezequiel@djobnet.com>
2025-02-03 21:05:04 -0400

    fix: section_weeks

--- src/routers/section_weeks.py
+++ src/routers/section_weeks.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from src.models.section_weeks import CreateSectionWeeksRequest
 from src.services.section_weeks import SectionWeeksService
+from sqlalchemy.orm import Session
+from src.database import get_db
 
-section_weeks_service = SectionWeeksService
 
 router = APIRouter(
     prefix="/section-weeks",
@@ -10,25 +11,37 @@ router = APIRouter(
 )
 
 @router.post("")
-async def create_section_week(section_weeks_request: CreateSectionWeeksRequest):
+async def create_section_week(section_weeks_request: CreateSectionWeeksRequest, db: Session = Depends(get_db)):
+    section_weeks_service = SectionWeeksService(db)
     return section_weeks_service.create_section_week(section_weeks_request)
 
+
 @router.get("")
-async def get_all_section_weeks():
+async def get_all_section_weeks(db: Session = Depends(get_db)):
+    section_weeks_service = SectionWeeksService(db)
     return section_weeks_service.get_all_section_weeks()
 
+
 @router.get("/section/{section_id}")
-async def get_weeks_by_section(section_id: int):
+async def get_weeks_by_section(section_id: int, db: Session = Depends(get_db)):
+    section_weeks_service = SectionWeeksService(db)
     return section_weeks_service.get_weeks_by_section(section_id)
 
+
 @router.get("/week/{week_id}")
-async def get_sections_by_week(week_id: int):
+async def get_sections_by_week(week_id: int, db: Session = Depends(get_db)):
+    section_weeks_service = SectionWeeksService(db)
     return section_weeks_service.get_sections_by_week(week_id)
 
+
 @router.put("")
-async def update_section_week(section_id: int, week_id: int, section_weeks_request: CreateSectionWeeksRequest):
+async def update_section_week(section_id: int, week_id: int,
+                               section_weeks_request: CreateSectionWeeksRequest, db: Session = Depends(get_db)):
+    section_weeks_service = SectionWeeksService(db)
     return section_weeks_service.update_section_week(section_id, week_id, section_weeks_request)
 
+
 @router.delete("")
-async def delete_section_week(section_id: int, week_id: int):
+async def delete_section_week(section_id: int, week_id: int, db: Session = Depends(get_db)):
+    section_weeks_service = SectionWeeksService(db)
     return section_weeks_service.delete_section_week(section_id, week_id)
