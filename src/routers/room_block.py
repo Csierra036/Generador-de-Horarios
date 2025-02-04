7376e9d75ed45cfbe6d13e5c2262458650a2175b
Ezequiel Bermudez <ezequiel@djobnet.com>
2025-02-03 21:09:49 -0400

    fix: room_block

--- src/routers/room_block.py
+++ src/routers/room_block.py
@@ -1,32 +1,35 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from src.models.room_block import CreateRoomBlockRequest
 from src.services.room_block import RoomBlockService
+from sqlalchemy.orm import Session
+from src.database import get_db
 
-# Instancia del servicio para room_block
-room_block_service = RoomBlockService
 
-# Configuración del router
 router = APIRouter(
     prefix="/room_block",
     tags=['RoomBlock']
 )
 
-# Endpoint para crear un registro en "room_block"
+
 @router.post("")
-async def create_room_block(room_block_request: CreateRoomBlockRequest):
+async def create_room_block(room_block_request: CreateRoomBlockRequest, db: Session = Depends(get_db)):
+    room_block_service = RoomBlockService(db)
     return room_block_service.create_room_block(room_block_request)
 
-# Endpoint para obtener todos los registros
+
 @router.get("")
-async def get_all_room_blocks():
+async def get_all_room_blocks(db: Session = Depends(get_db)):
+    room_block_service = RoomBlockService(db)
     return room_block_service.get_all_room_blocks()
 
-# Endpoint para actualizar un registro existente
+
 @router.put("")
-async def update_room_block(room_block_id: int, room_block_request: CreateRoomBlockRequest):
+async def update_room_block(room_block_id: int, room_block_request: CreateRoomBlockRequest, db: Session = Depends(get_db)):
+    room_block_service = RoomBlockService(db)
     return room_block_service.update_room_block(room_block_id, room_block_request)
 
-# Endpoint para eliminar un registro
+
 @router.delete("")
-async def delete_room_block(room_block_id: int):
+async def delete_room_block(room_block_id: int, db: Session = Depends(get_db)):
+    room_block_service = RoomBlockService(db)
     return room_block_service.delete_room_block(room_block_id)
