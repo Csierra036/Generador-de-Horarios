01161d6ad66184f6908b16f286c2a722f116c6ab
Ezequiel Bermudez <ezequiel@djobnet.com>
2025-02-03 21:01:34 -0400

    fix: seaction

--- src/routers/section.py
+++ src/routers/section.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from src.models.section import CreateSectionRequest
 from src.services.section import SectionService
+from sqlalchemy.orm import Session
+from src.database import get_db
 
-section_service = SectionService
 
 router = APIRouter(
     prefix="/section",
@@ -10,17 +11,24 @@ router = APIRouter(
 )
 
 @router.post("")
-async def create_section(section_request: CreateSectionRequest):
+async def create_section(section_request: CreateSectionRequest, db: Session = Depends(get_db)):
+    section_service = SectionService(db)
     return section_service.create_section(section_request)
 
+
 @router.get("")
-async def get_all_sections():
+async def get_all_sections(db: Session = Depends(get_db)):
+    section_service = SectionService(db)
     return section_service.get_all_sections
 
+
 @router.put("")
-async def update_section(section_id: int, section_request: CreateSectionRequest):
+async def update_section(section_id: int, section_request: CreateSectionRequest, db: Session = Depends(get_db)):
+    section_service = SectionService(db)
     return section_service.update_section(section_id, section_request)
 
+
 @router.delete("")
-async def delete_section(section_id: int):
+async def delete_section(section_id: int, db: Session = Depends(get_db)):
+    section_service = SectionService(db)
     return section_service.delete_section(section_id)
